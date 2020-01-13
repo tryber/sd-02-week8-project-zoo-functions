9fb9e0e27c7ade9841e43dd64caf544127a5a3b3
Douglas Henrique <doughrqe@gmail.com>
2020-01-13 15:46:06 -0300

    Adequação Code Climate

--- src/zoo.js
+++ src/zoo.js
@@ -1,11 +1,11 @@
 const data = require('./data')
 
 function entryCalculator(entrants) {
-  if( entrants === undefined || JSON.stringify(entrants) === '{}' ){
+  if (entrants === undefined || JSON.stringify(entrants) === '{}') {
     return 0;
   }
-  const {Adult , Child , Senior} = entrants;
-  return ( Adult * 49.99 ) + ( Child * 20.99 ) + ( Senior * 24.99 );
+  const { Adult, Child, Senior } = entrants;
+  return (Adult * 49.99) + (Child * 20.99) + (Senior * 24.99);
 };
 
 function schedule(dayName) {
