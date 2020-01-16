42475ac16c589e06fe23fb1f16607be411c555cb
fernandoluckesi <fernando.luckesi94@gmail.com>
2020-01-16 04:04:50 -0300

    Zerando CC Versão 1.0.0.0

--- src/zoo.js
+++ src/zoo.js
@@ -1,41 +1,40 @@
 const data = require('./data')
 
 function entryCalculator(entrants) {
-  if ( Object.keys( entrants ).length === 0 ){
-  return 0;
+  if (Object.keys(entrants).length === 0){
+    return 0;
   } else {
-    return (Object.values(entrants)[0] * 46.985) + (Object.values(entrants)[1] * 23.4925) + (Object.values(entrants)[2] * 23.4925);
+      return (Object.values(entrants)[0] * 46.985) + (Object.values(entrants)[1] * 23.4925) + (Object.values(entrants)[2] * 23.4925);
   }
 };
 
 function schedule(dayName) {
   const newObj = {};
-  if ( dayName === null ){
-      return 0;
-  } else {
-      Object.keys(data.hours).forEach((key) => {
-         if ( key === dayName ) {
-             newObj[key] = data.hours[key];
-             console.log(newObj)
-           }
-       });
-      return newObj;
-    }
-};
+  if (dayName === null){
+    return 0;
+  };
+    Object.keys(data.hours).forEach((key) => {
+        if (key === dayName) {
+           newObj[key] = data.hours[key];
+           console.log(newObj)
+         }
+      });
+    return newObj;
+  };
 
 function animalCount(species) {
-  const obj ={};
-  if ( species === undefined ){
-      const contagem = animals.forEach(item => {
-          obj[item.name] = item.residents.length;
-      });
-      return obj;      
+  const obj = {};
+  if (species === undefined){
+      const contagem = data.animals.forEach((item) => {
+    obj[item.name] = item.residents.length;
+  });
+      return obj; 
   } else {
       const contagem = animals.filter(({name}) => {
           return name === species;
       });
       return contagem[0].residents.length;
-  }
+  };
 };
 
 function animalMap(options) {
@@ -48,11 +47,11 @@ function animalMap(options) {
     let objMain = {};
     const atribuirKeys = keys.forEach(element => {
       const localizarNome = data.animals.filter(({location}) => location === element);
-      objMain[element] = localizarNome.reduce((acumulador, {name}) => 
+      objMain[element] = localizarNome.reduce((acumulador, {name}) =>
       acumulador.concat(name), []);
     });
   return objMain;
-  }  
+  }
 };
 
 function animalPopularity(rating) {
