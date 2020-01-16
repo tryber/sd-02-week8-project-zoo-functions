f903ad5ff7a4c3062b223132c461db40563585d6
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-01-15 23:17:23 -0200

    Finalizado emplyeeCovarage

--- src/zoo.js
+++ src/zoo.js
@@ -19,7 +19,7 @@ const schedule = (dayName) => {
       ? 'CLOSED'
       : `Open from ${open}am until ${close - 12}pm`
     acc[day] = text;
-      return acc;
+    return acc;
   }, {});
 };
 
@@ -27,22 +27,22 @@ const schedule = (dayName) => {
 const animalCount = (species) => {
   const [...args] = data.animals;
   const arrFinal = args.reduce((acc, cur) =>
-  ({ ...acc, [cur.name]: Object.keys(cur.residents).length }), {})
+    ({ ...acc, [cur.name]: Object.keys(cur.residents).length }), {})
   if (species === null || species === undefined) {
     return arrFinal;
   }
   return arrFinal[species];
 };
 
-function animalMap(options) { 
-  const arrFinal = data.animals.reduce ( (arr, cur) => {
-    if ( arr[cur.location] === undefined ) {
+function animalMap(options) {
+  const arrFinal = data.animals.reduce((arr, cur) => {
+    if (arr[cur.location] === undefined) {
       arr[cur.location] = [];
     }
     arr[cur.location] = [...arr[cur.location], cur.name]
     return arr;
-  },{})
-  console.log (arrFinal);
+  }, {})
+  console.log(arrFinal);
 };
 
 //animalMap();
@@ -54,8 +54,8 @@ function animalPopularity(rating) {
 function animalsByIds(...ids) {
   if (ids[0] === undefined)
     return [];
-  const arrFinal = ids.reduce ( (arr, cur) => ([
-  ...arr, data.animals.find ( el => el.id === cur )
+  const arrFinal = ids.reduce((arr, cur) => ([
+    ...arr, data.animals.find(el => el.id === cur)
   ]), [])
   return arrFinal;
 };
@@ -69,8 +69,8 @@ function employeesByIds(ids) {
 };
 
 function employeeByName(names = {}) {
-  if (names[0] !== undefined ) {
-    names = data.employees.find ( el => el.lastName === names || el.firstName === names )
+  if (names[0] !== undefined) {
+    names = data.employees.find(el => el.lastName === names || el.firstName === names)
   }
   return names;
 };
@@ -79,49 +79,61 @@ function managersForEmployee(idOrName) {
   // seu código aqui
 };
 
-function employeeCoverage(idOrName) {
-  };
+function employeeCoverage(str) {
+  if (str === undefined) {
+    return (data.employees.reduce((arr, cur) => {
+      arr[`${cur.firstName} ${cur.lastName}`] = cur.responsibleFor.map(ele =>
+        data.animals.find(el => el.id === ele).name)
+      return arr
+    }, {}))
+  }  
+  const arrai = data.employees.find(el =>
+    str === el.id || str === el.firstName || str === el.lastName);
+  const arrFinal = arrai.responsibleFor.map (ele => data.animals.find (el =>
+    ele === el.id).name);
+  return {[`${arrai.firstName} ${arrai.lastName}`]: arrFinal};
+};
 
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  return data.employees.push ({id, firstName, lastName, managers, responsibleFor});
+  return data.employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function isManager(id) {
-  return data.employees.some ( ele => ele.managers.some ( el => el === id ))
+  return data.employees.some(ele => ele.managers.some(el => el === id))
 }
 
 function animalsOlderThan(animal, age) {
- return data.animals.find (specie =>
- specie.name === animal ).residents.every (animalAge => animalAge.age >= age);
+  return data.animals.find(specie =>
+    specie.name === animal).residents.every(animalAge => animalAge.age >= age);
 }
 
 function oldestFromFirstSpecies(id) {
-  const specie = data.employees.find (ele => ele.id === id).responsibleFor[0];
-  return Object.values ((data.animals.find (el =>
-     el.id === specie)).residents.sort ((a, b) =>
-    b.age - a.age)[0])
+  const specie = data.employees.find(ele => ele.id === id).responsibleFor[0];
+  return Object.values((data.animals.find(el =>
+    el.id === specie)).residents.sort((a, b) =>
+      b.age - a.age)[0])
 }
 
 function increasePrices(percentage) {
-  const value = ((percentage/100) + 1)*100;
-  const  teste = Object.keys (data.prices).reduce ((acc, cur) => {
-    acc[cur] = Math.ceil (data.prices[cur] * value) / 100;
-    return acc; 
+  const value = ((percentage / 100) + 1) * 100;
+  const teste = Object.keys(data.prices).reduce((acc, cur) => {
+    acc[cur] = Math.ceil(data.prices[cur] * value) / 100;
+    return acc;
   }, {})
   data.prices.Adult = teste.Adult;
   data.prices.Child = teste.Child;
-  data.prices.Senior = teste.Senior;  
+  data.prices.Senior = teste.Senior;
 }
 
-class Animal {  
-  constructor (name = '', sex = 'male', age = 0, species = '') {
+class Animal {
+  constructor(name = '', sex = 'male', age = 0, species = '') {
     this.name = name;
     this.sex = sex;
     this.age = age;
     this.species = species.slice(0, -1);
   }
-  info () {
+  info() {
     return `${this.name} is a ${this.age} year old ${this.sex} ${this.species}`
   }
   static get Total() {
@@ -130,16 +142,16 @@ class Animal {
   static set Total(v) {
     this.total = v;
   }
-  static totalAnimals () {
+  static totalAnimals() {
     return this.total;
   }
 }
 
 function createAnimals() {
   let animals = [];
-  data.animals.forEach (animais => {
-    animais.residents.forEach ((el) => {
-      animals.push(new Animal (el.name, el.sex, el.age, animais.name));
+  data.animals.forEach(animais => {
+    animais.residents.forEach((el) => {
+      animals.push(new Animal(el.name, el.sex, el.age, animais.name));
     })
   })
   Animal.Total = animals.length;
