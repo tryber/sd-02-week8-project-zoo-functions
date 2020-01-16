30b0cdb11e564b4c185cbfd431beb60b580debe3
Lucas-Werner <lucas.1werner@gmail.com>
2020-01-16 13:44:28 -0300

    projeto incompleto

--- src/zoo.js
+++ src/zoo.js
@@ -1,15 +1,46 @@
 const data = require('./data')
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  if (Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const {Adult, Child, Senior} = entrants
+  const entrada = [Adult, Senior, Child]
+  const preco = Object.values(data.prices)
+   let resultado = 0
+   for (let i = 0; i < entrada.length; i++){
+    let multiplica = entrada[i] * preco[i]
+    resultado += multiplica
+  }
+  return resultado
 };
 
 function schedule(dayName) {
-  // seu código aqui
+  const days = dayName ? [dayName] : Object.keys(data.hours);
+    return days.reduce((acc, day) => {
+    const { open, close } = data.hours[day];
+    const text = day === 'Monday'
+      ? 'CLOSED'
+      : `Open from ${open}am until ${close - 12}pm`
+    acc[day] = text;
+    return acc;
+  }, {});
 };
 
 function animalCount(species) {
-  // seu código aqui
+  const nomes = data.animals.map(e => e.name)
+  const quantidade = data.animals.map(e => e.residents.length)
+  const obj = {}
+  nomes.forEach((nome, index) => {
+    obj[nome] = quantidade[index]
+    })
+  if (species === null || species === undefined) {
+    return obj;
+  }
+  return obj[species];
 };
 
 function animalMap(options) {
@@ -21,11 +52,17 @@ function animalPopularity(rating) {
 };
 
 function animalsByIds(ids) {
-  // seu código aqui
+  if (ids === null || ids === undefined) {
+    return []
+  }
+  return ids.map(arg => data.animals.find(item => item.id === arg));
 };
 
 function animalByName(animalName) {
-  // seu código aqui
+  if (employeeName === null || employeeName === undefined) {
+    return {}
+  }
+  return data.employees.find((e => e.firstName === employeeName || e.lastName === employeeName))
 };
 
 function employeesByIds(ids) {
@@ -41,7 +78,22 @@ function managersForEmployee(idOrName) {
 };
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  const obj = {}
+   data.employees.forEach(e => {
+    obj[`${e.firstName} ${e.lastName}`] =
+    e.responsibleFor.map(id => data.animals.find(animal => animal.id === id).name)
+   })
+  const inputFuncionario = input => {
+    const funcionario = data.employees.find(e => e.id === input || e.firstName === input || e.lastName === input)
+    return `${funcionario.firstName} ${funcionario.lastName}`
+  }
+  if (idOrName === null || idOrName === undefined) {
+    return obj
+  } else {
+    const obj1 = {}
+    obj1[inputFuncionario(idOrName)] = obj[inputFuncionario(idOrName)]
+    return obj1
+  }
 };
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
