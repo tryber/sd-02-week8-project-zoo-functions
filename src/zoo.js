4839f85dc02b25009cf5357400443b6caae4e1c1
Giorgia <giorgialeoniac@gmail.com>
2020-01-20 13:00:56 -0300

    Projeto completo

--- src/zoo.js
+++ src/zoo.js
@@ -66,13 +66,42 @@ function animalCount(species) {
   return zooList[species];
 }
 
-function animalMap(options) {
-  // seu código aqui
-  // sem parâmetros, retorna animais categorizados por localização
-  // com opções especificadas, retorna nomes de animais
-  // com opções especificadas, retorna nomes de animais ordenados
-  // com oções especificadas, retorna somente nomes de animais macho/fêmea
-  // só retorna informações específicas de gênero se includeNames for setado
+function animalsFilteredBySex(animals, sex) {
+  if (!sex) return animals
+  return animals.filter(({
+    sex: animalSex
+  }) => sex === animalSex)
+}
+
+function sortedAnimalNames(animalNames, sorted) {
+  if (!sorted) return animalNames
+  return animalNames.sort()
+}
+
+function animalMap(options = {}) {
+  const {
+    includeNames,
+    sex,
+    sorted
+  } = options
+  return data.animals.reduce((obj, animal) => {
+    const result = obj
+    const animalsInLocation = obj[animal.location] || []
+    if (!includeNames) {
+      result[animal.location] = [...animalsInLocation, animal.name]
+      return result
+    }
+    let animals = animal.residents
+    animals = animalsFilteredBySex(animals, sex)
+    let animalNames = animals.map(({
+      name
+    }) => name)
+    animalNames = sortedAnimalNames(animalNames, sorted)
+    result[animal.location] = [...animalsInLocation, {
+      [animal.name]: animalNames
+    }]
+    return result
+  }, {})
 };
 
 function animalPopularity(rating) {
