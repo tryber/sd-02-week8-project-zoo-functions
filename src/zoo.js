0cd739ceecd93a402afb902ccba2ab057951fe8c
Giorgia <giorgialeoniac@gmail.com>
2020-01-20 13:07:42 -0300

    Removendo comentários desnecessários, código finalizado

--- src/zoo.js
+++ src/zoo.js
@@ -1,16 +1,12 @@
 const data = require('./data')
 
 function entryCalculator(entrants) {
-  // retorna 0 se nenhum argumento for passado
   if (entrants === undefined) {
     return 0;
   }
-  // retorna 0 se um objeto vazio for passado
   if (Object.values(entrants).length === 0) {
     return 0;
   }
-  // retorna o preço total a ser cobrado dado o número de adultos, crianças e
-  // idosos
   const {
     Child,
     Adult,
@@ -26,9 +22,6 @@ function entryCalculator(entrants) {
 
 function schedule(dayName) {
   // seu código aqui
-  // sem parâmetros, retorna um cronograma legível para humanos
-  // se um único dia for passado, retorna somente este dia em um formato
-  // legível para humanos
   const days = dayName ? [dayName] : Object.keys(data.hours);
   return days.reduce((acc, day) => {
     const {
@@ -53,7 +46,6 @@ const catchingAnimals = (who, howMany) => {
 
 function animalCount(species) {
   // seu código aqui
-  // Sem parâmetros, retorna animais e suas quantidades
   const names = data.animals.map(element => element.name);
   const quantity = data.animals.map(element => element.residents.length);
   const zooList = catchingAnimals(names, quantity);
@@ -61,8 +53,6 @@ function animalCount(species) {
   if (species === undefined) {
     return zooList;
   }
-
-  // com o nome de uma espécide de animal, retorna somente a quantidade
   return zooList[species];
 }
 
@@ -111,7 +101,6 @@ function animalPopularity(rating) {
 
 function animalsByIds(ids, id2 = '') {
   // seu código aqui
-  // sem parâmetros, retorna um array vazio
   const nothing = [];
   if (ids && id2 === undefined) {
     return nothing
@@ -136,12 +125,9 @@ function employeesByIds(ids) {
 
 function employeeByName(employeeName) {
   // seu código aqui
-  // sem parâmetros, retorna um objeto vazio
   if (employeeName === undefined) {
     return {};
   }
-  // quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
-  // quando provido o último nome do funcionário, retorna o objeto do funcionário
   return data.employees.find(element =>
     employeeName === element.firstName || employeeName === element.lastName);
 };
@@ -171,7 +157,6 @@ function employeeCoverage(resp) {
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
-  // adiciona um funcionário no fim da lista
   return data.employees.push({
     id,
     firstName,
@@ -183,23 +168,17 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function isManager(id) {
   // seu código aqui
-  // testa se o id passado é de um gerente
   return data.employees.some(algum => algum.managers.some(alguem => alguem === id))
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
-  // passados o nome de uma espécie e uma idade, testa se todos os animais desta
-  // espécie possuem a idade mínima especificada (todos = every)
   return data.animals.find(who =>
     who.name === animal).residents.every(hisAge => hisAge.age >= age);
 }
 
 function oldestFromFirstSpecies(id) {
   // seu código aqui
-  // passado o id de um funcionário, encontra a primeira espécie de animal
-  // gerenciado pelo funcionáio, e retorna um array com nome, sexo e idade do
-  // animal mais velho dessa espécie
   const animalsEmp = data.employees.find(pesq => pesq.id === id).responsibleFor[0];
   const result = data.animals.find(animal => animal.id === animalsEmp);
   const older = result.residents.sort((young, old) => old.age - young.age)[0];
@@ -208,7 +187,6 @@ function oldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
-  // data uma porcentagem, incrementa todos os preços, arrendondados em duas casas
   const valor = ((percentage / 100) + 1) * 100;
   const [adultPrices, seniorPrices, childPrices] = Object.values(data.prices);
   data.prices.Adult = Math.ceil(adultPrices * valor) / 100;
